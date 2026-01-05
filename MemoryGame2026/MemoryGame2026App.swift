a7608620fe75fe6011228a1d8afdf6541074c7a7
Devinder <ics25-009@camosun.ca>
2026-01-05 15:38:13 -0800

    Initial Commit

---
+++ MemoryGame2026/MemoryGame2026App.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGame2026App.swift
+//  MemoryGame2026
+//
+//  Created by  on 2026-01-05.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGame2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryGame2026Tests/MemoryGame2026Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGame2026Tests.swift
+//  MemoryGame2026Tests
+//
+//  Created by  on 2026-01-05.
+//
+
+import Testing
+@testable import MemoryGame2026
+
+struct MemoryGame2026Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
