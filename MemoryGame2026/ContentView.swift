649cf96f9a324c4b203e6ca22e064588efa21d1b
Devinder <ics25-009@camosun.ca>
2026-01-06 10:50:14 -0800

    Initial UI

--- MemoryGame2026/ContentView.swift
+++ MemoryGame2026/ContentView.swift
@@ -8,12 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    let images = ["pencil", "eraser", "scribble", "lasso"]
+    
+    @State private var curIndex = 0
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Image(systemName: "arrowtriangle.left")
+                    .resizable().aspectRatio(contentMode: .fit)
+                    .frame(width: 60)
+                    .padding(.horizontal, 17)
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    .onTapGesture{
+                      curIndex = (curIndex - 1 + images.count) % images.count
+                    }
+               
+                  Image(systemName: images[curIndex])
+                        .resizable().aspectRatio(contentMode: .fit)
+                        .imageScale(.small)
+                        .foregroundStyle(.black)
+                
+                    
+                
+                Image(systemName: "arrowtriangle.right")
+                    .resizable().aspectRatio(contentMode: .fit)
+                    .frame(width: 60)
+                    .padding(.horizontal, 17)
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    .onTapGesture {
+                        curIndex = (curIndex + 1) % images.count
+                    }
+            }
+                Text(images[curIndex])
+            
         }
         .padding()
     }
@@ -22,3 +52,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
