81db25722339efe6dc49087811e341e7e46539e9
Devinder <ics25-009@camosun.ca>
2026-01-19 15:04:54 -0800

    Chnages

--- MemoryGame2026/ContentView.swift
+++ MemoryGame2026/ContentView.swift
@@ -8,15 +8,35 @@ struct ContentView: View {
     @AppStorage("curIndex") private var curIndex = 0
     @AppStorage("Bonus") private var Bonus: Bool = false
     @AppStorage("value") private var value = 0
+    @State var images2 = ["gear", "house"]
+    @State var index = 0
     var body: some View {
-        SettingsView()
-    }
-
     
-    // MARK: - Actions
+        NavigationStack{
+                        VStack{
+                            if index == 0{
+                                SettingsView()
+                                
+                            }else{
+                                GameView()
+                            }
+                        }
+                    }
+                    .toolbar{
+                        ToolbarItemGroup{
+                            Button(action: {
+                                index = (index + 1) % images2.count
+                            },label: {
+                                Image(systemName: images2[(index + 1) % images2.count])
+                            })
+                        }
+                    }
+                }
+            }
+        
+    
    
-   
-}
+
 
 
 #Preview {
--- MemoryGame2026/MemoryGame2026App.swift
+++ MemoryGame2026/MemoryGame2026App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MemoryGame2026App: App {
     var body: some Scene {
         WindowGroup {
-            GameView()
+            ContentView()
         }
     }
 }
